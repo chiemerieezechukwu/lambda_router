bc41fbfb49f5c4b09032c16e92600329c1a9ef3b
Chiemerie Ezechukwu <50174206+chiemerieezechukwu@users.noreply.github.com>
2022-04-17 00:32:22 +0200

    add multi query params support

--- lambda_router/helpers/api_request.py
+++ lambda_router/helpers/api_request.py
@@ -7,7 +7,13 @@ class ApiRequest:
         self.context = context
 
     def query_params(self):
-        return self.event.get("queryStringParameters")
+        _ret = {}
+        for key, value in self.event.get("queryStringParameters", {}).items():
+            if len(value.split(",")) == 1:
+                _ret[key] = value.split(",")[0]
+            else:
+                _ret[key] = value.split(",")
+        return _ret
 
     def request_context(self):
         return self.event["requestContext"]
@@ -26,6 +32,10 @@ class ApiRequest:
     @property
     def method(self):
         return self.http().get("method", "GET").upper()
+
+    @property
+    def args(self):
+        return self.query_params()
 
     def get_method(self):
         return self.event["httpMethod"]
